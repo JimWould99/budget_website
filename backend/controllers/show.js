7ad3b667adc7ae97a7ebb4dce127add087c95c3b
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-09-11 09:38:20 +0100

    initial commit of skeleton of the backend

---
+++ backend/controllers/show.js
@@ -0,0 +1,7 @@
+exports.display_budget = async (req, res) => {
+  res.json({ mssg: "Not yet implemented display budget" });
+};
+
+exports.display_expense = async (req, res) => {
+  res.json({ mssg: "Not yet implemented" });
+};
